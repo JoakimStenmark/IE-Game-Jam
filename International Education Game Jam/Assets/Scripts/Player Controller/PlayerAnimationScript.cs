8ef797fd13252082c890a50ec1d93db0a0b97d03
GLUStudent200557 <78432932+GLUStudent200557@users.noreply.github.com>
2021-02-04 17:08:14 +0100

    Player Animation

--- International Education Game Jam/Assets/Scripts/Player Controller/PlayerAnimationScript.cs
+++ International Education Game Jam/Assets/Scripts/Player Controller/PlayerAnimationScript.cs
@@ -4,23 +4,45 @@ using UnityEngine;
 
 public class PlayerAnimationScript : MonoBehaviour
 {
-    private PlayerMovement playerMovement;
+    private PlayerMovement playerScript;
     private Animator playerAnimation;
+
+    [SerializeField] private float maxWhipingAnimTime;
 
     void Start()
     {
-        playerMovement = GetComponent<PlayerMovement>();
-        playerMovement.playerState = PlayerState.Idle; 
+        playerScript = GetComponent<PlayerMovement>();
+        playerAnimation = GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (playerMovement.ReturnDirection().x == 0 && playerMovement.ReturnDirection().y == 0)
+        if (playerScript.ReturnDirection().x == 0 && playerScript.ReturnDirection().y == 0)
         {
-            playerMovement.playerState = PlayerState.Idle;
-            playerAnimation.SetInteger("playerState", (int)playerMovement.playerState);
+            playerScript.playerState = PlayerState.Idle;
+            playerAnimation.SetInteger("playerState", (int)playerScript.playerState);
         }
+
+        if (playerScript.itemHeld == ItemHeld.Wiper && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerScript.playerState = PlayerState.Wiping;
+            playerAnimation.SetTrigger("wipeAnim");
+        }
+
+        if(playerScript.itemHeld == ItemHeld.Spray && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerScript.playerState = PlayerState.Spraying;
+            playerAnimation.SetTrigger("sprayAnim");
+        }
+
+        if (playerScript.itemHeld == ItemHeld.Broom && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerScript.playerState = PlayerState.WhackingBroom;
+            playerAnimation.SetTrigger("whackAnim");
+        }
+
+
     }
 
-   
+
 }
